d402073694a3fa9d243197593ee8cc0d6348de05
David Cabrera <davecabrera1992@gmail.com>
2021-11-18 14:12:13 -0600

    Add API Keys, Setup shell for networking

---
+++ AllTrailsLunch/Networking/GetNearbyPlacesNetworkRequest.swift
@@ -0,0 +1,14 @@
+//
+//  GetPlacesNetworkRequest.swift
+//  AllTrailsLunch
+//
+//  Created by David Cabrera on 11/18/21.
+//
+
+import Foundation
+
+
+//TODO: Make into protocol
+struct GetNearbyPlacesNetworkRequest {
+    init(location: )
+}
---
+++ AllTrailsLunch/Networking/NetworkClient.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkClient.swift
+//  AllTrailsLunch
+//
+//  Created by David Cabrera on 11/18/21.
+//
+
+import Foundation
+import Combine
+
+
+
